20d20335e0cfad175e6637ae1324c467a3af9f86
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-09-03 20:57:06 +0545

    Created function to generate random title from rock scissor paper

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+    let gameValues = ['Rock', 'Paper', 'Scissor'];
+
+    let randomNumber = Math.floor(Math.random()*3);
+
+    console.log(gameValues[randomNumber])
+
+    // return gameValues[randomNumber];
+}
